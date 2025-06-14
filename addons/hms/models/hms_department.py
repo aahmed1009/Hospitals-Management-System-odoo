d5bb253e80577f68349fe4b4e9aedede7524f75b
Alaa Ahmed <3la2.a7med40@gmail.com>
2025-06-14 18:56:16 +0300

    improved ui

--- addons/hms/models/hms_department.py
+++ addons/hms/models/hms_department.py
@@ -8,3 +8,4 @@ class HmsDepartment(models.Model):
     capacity = fields.Integer(string='Capacity')
     is_opened = fields.Boolean(string='Is Opened', default=True)
     patient_ids = fields.One2many('hms.patient', 'department_id', string='Patients')
+    doctor_ids = fields.Many2many('hms.doctor', string='Doctors')
--- addons/hms/models/hms_doctor.py
+++ addons/hms/models/hms_doctor.py
@@ -3,8 +3,12 @@ from odoo import models, fields
 class HmsDoctor(models.Model):
     _name = 'hms.doctor'
     _description = 'Doctor'
+    _rec_name = 'first_name'
 
     first_name = fields.Char(string='First Name', required=True)
     last_name = fields.Char(string='Last Name', required=True)
     image = fields.Binary(string='Image')
     patient_ids = fields.Many2many('hms.patient', string='Patients')
+    department_ids = fields.Many2many('hms.department', string='Departments')
+
+    
